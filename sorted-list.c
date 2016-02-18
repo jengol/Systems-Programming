e84a1a2c672892d36a12eeac12998a8e2c3ef00d
[j.liao5079@gmail.com] <j.liao5079@gmail.com>
2016-02-18 16:58:40 -0500

    something

--- sorted-list.c
+++ sorted-list.c
@@ -1,11 +1,37 @@
-//
-//  sorted-list.c
-//  
-//
-//  Created by John Eng on 2/17/16.
-//
-//
+//Jamie will do SL functions 
+SortedListPtr SLCreate(CompareFuncT cf, DestructFuncT df){
+  SortedListPtr SLptr = (SortedListPtr *)malloc(sizeof(SortedListPtr));
 
-#include <stdio.h>
-#include "sorted-list.h"
 
+}
+
+void SLDestroy(SortedListPtr list){
+  free(list);
+  return;
+}
+
+int SLInsert(SortedListPtr list, void *newObj){
+ 
+}
+
+int SlRemove(SortedListPtr list, void *newObj){
+ //redirect the next ptr of SortedList to point to .next.next
+}
+
+
+//John will do these :D
+SortedListIteratorPtr SLCreateIterator(SortedListPtr list){
+
+}
+
+void SLDestroyIterator(SortedListIteratorPtr iter){
+  free(iter);
+  return;
+}
+
+void *SLNextItem(SortedListIteratorPtr iter){
+
+}
+
+void *SLGetItem(SortedListIteratorPtr iter){
+}
