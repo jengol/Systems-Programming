99b80fd4938510921ef9ed2859bbddcba284dee1
John Eng <John@nbp-63-145.nbp.ruw.rutgers.edu>
2016-02-18 16:44:45 -0500

    First commit

---
+++ main.c
@@ -0,0 +1,25 @@
+//
+//  main.c
+//  
+//
+//  Created by John Eng on 2/17/16.
+//
+//
+
+#include <stdio.h>
+#include "sorted-list.h"
+
+
+
+int main(){
+    
+    
+    
+    
+    
+    
+    
+    
+    printf("Test");
+    return 0;
+}
---
+++ sorted-list.c
@@ -0,0 +1,11 @@
+//
+//  sorted-list.c
+//  
+//
+//  Created by John Eng on 2/17/16.
+//
+//
+
+#include <stdio.h>
+#include "sorted-list.h"
+
